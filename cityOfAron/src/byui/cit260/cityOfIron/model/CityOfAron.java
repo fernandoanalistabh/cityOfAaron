f2c64605aa718793d8584fcb7349fd533eb17fac
Fernando e Andre <Allex@Allex-PC>
2018-10-11 23:50:57 -0300

    Matheus entrando no projeto

---
+++ cityOfAron/src/byui/cit260/cityOfIron/model/CityOfAron.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.cityOfIron.model;
+
+/**
+ *
+ * @author Fernando e Andre
+ */
+public class CityOfAron {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Player newPlayer = new Player();
+        newPlayer.setName("João");
+        String newPlayerName = newPlayer.getName(); 
+        System.out.println(newPlayerName);
+        System.out.println(newPlayer.toString());
+        System.out.println(TeamMember.Andre.getName());
+        System.out.println(TeamMember.Andre.getTitle());
+    }
+    
+}
