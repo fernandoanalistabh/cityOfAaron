9d94f5d58930d1f8b93bded9b4ac29c787f4f1fe
Fernando e Andre <>
2018-10-11 23:30:58 -0300

    Matheus entrando no projeto

---
+++ cityOfAron/src/byui/cit260/cityOfIron/model/Player.java
@@ -0,0 +1,15 @@
+/*
+* The Player() class file for the cityOfAaron project
+* CIT-260
+* Fall 2018
+* Team members: Andre Matheus, Fernando Santos, Vinicius
+*/
+package byui.cit260.cityOfIron.model;
+
+/**
+ *
+ * @author Allex
+ */
+public class Player {
+    
+}
