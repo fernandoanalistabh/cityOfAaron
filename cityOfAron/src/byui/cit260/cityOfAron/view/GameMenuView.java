439401f4aa357592a88d51be21f49e7ae794f2c6
fernandoanalistabh <fernando.analistabh@gmail.com>
2018-11-17 20:29:58 -0200

    Game Menu extends main menu

--- cityOfAron/src/byui/cit260/cityOfAron/view/GameMenuView.java
+++ cityOfAron/src/byui/cit260/cityOfAron/view/GameMenuView.java
@@ -6,14 +6,12 @@ import java.util.Scanner;
 /**
  * @author Fernando
  */
-public class GameMenuView {
-    String gameMenu;
-    int max;
-    Scanner keyboard = new Scanner(System.in);
+public class GameMenuView extends MenuView{
+      
     Game theGame;
        
     public GameMenuView() {
-        gameMenu = "\n"+
+        super("\n"+
                   "********************************\n" +
                   "* CITY OF AARON: THE GAME MENU *\n" +
                   "********************************\n" +
@@ -21,16 +19,15 @@ public class GameMenuView {
                   " 2 – View/Print a list\n" +
                   " 3 – Move to a new location\n" +
                   " 4 – Manage the Crops\n" +
-                  " 5 – Return to the Main menu\n";
-        max = 5;
+                  " 5 – Return to the Main menu\n", 5);
     }
 
     public String getGameMenu() {
-        return gameMenu;
+        return menu;
     }
 
     public void setGameMenu(String gameMenu) {
-        this.gameMenu = gameMenu;
+        this.menu = gameMenu;
     }
 
     public int getMax() {
@@ -47,56 +44,6 @@ public class GameMenuView {
 
     public void setTheGame(Game theGame) {
         this.theGame = theGame;
-    }
-    
-    /**
-    * The displayMenuView method
-    * Purpose: displays the menu, gets the user's input, and does the
-    *          selected action
-    * Parameters: none
-    * Returns: none
-    */
-    // ========================================================= 
-    public void displayMenuView()
-    {
-        int menuOption;
-        do
-        {
-            // Display the menu
-            System.out.println(getGameMenu());
-            // Prompt the user and get the user’s input
-            menuOption = getMenuOption();
-            // Perform the desired action
-            doAction(menuOption);
-            // Determine and display the next view
-        } while (menuOption != max);
-    }
-    
-    /**
-    * The getMenuOption method
-    * Purpose: gets the user's input
-    * Parameters: none
-    * Returns: integer - the option selected
-    */
-    // =================================== 
-    public int getMenuOption()
-    {
-        // declare a variable to hold user’s input
-        int userInput;
-        // begin loop
-        do
-        {    
-            // get user input from the keyboard
-            userInput = keyboard.nextInt();
-            // if it is not a valid value, output an error message
-            if (userInput < 1 || userInput > max)
-            {
-                System.out.println("Option must be between 1 and " + max);
-            }
-        // loop back to the top if input was not valid
-        } while (userInput < 1 || userInput > max);
-        // return the value input by the user
-        return userInput;
     }
     
     /**
@@ -140,7 +87,6 @@ public class GameMenuView {
     }
     
     void manageCrops(){
-        CropView.runCropsView(); //O professor descontou pontos do meu L08 individual
-                                 //assgniment por não chamar o runCropsView aqui (André)
+        CropView.runCropsView();
     }
 }
