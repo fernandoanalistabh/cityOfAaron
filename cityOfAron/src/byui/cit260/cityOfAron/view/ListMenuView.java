579c33520914081e5959f232a9524f74c9e892c8
Andre Pedrosa <Allex@Allex-PC>
2018-12-09 00:25:43 -0200

    Created saveProvisions() method in ListMenuView class Updated listProvisions() method in ListMenuView class

--- cityOfAron/src/byui/cit260/cityOfAron/view/ListMenuView.java
+++ cityOfAron/src/byui/cit260/cityOfAron/view/ListMenuView.java
@@ -8,6 +8,10 @@ package byui.cit260.cityOfAron.view;
 import byui.cit260.cityOfAron.control.GameControl;
 import byui.cit260.cityOfAron.model.Game;
 import byui.cit260.cityOfAron.model.ListItem;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.FileReader;
+import java.io.BufferedReader;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -29,13 +33,14 @@ public class ListMenuView {
                         "****************************\n" +
                         "1 - List or view the animals in the storehouse.\n"+
                         "2 - List or view the tools in the storehouse.\n"+
-                        "3 - List or view the provisions in the storehouse.\n"+
-                        "4 - List or view the authors of this game.\n"+
-                        "5 - Return\n"; //This last option don't show in the
+                        "3 - Save the provitions to a listfile\n"+
+                        "4 -   List or view the provisions in the storehouse.\n"+
+                        "5 - List or view the authors of this game.\n"+
+                        "6 - Return\n"; //This last option don't show in the
                                         //requirements document, but I though it 
                                         //would be more consistent to add it
                                         //once the other menus have.
-        this.max = 5;
+        this.max = 6;
     }
 
 
@@ -86,13 +91,16 @@ public void displayMenuView()
             case 2:// if the option is 2, call listTools( )
                 listTools();
                 break;
-            case 3:// if the option is 3, call listProvisions( )
+            case 3:// if the option is 3, call saveProvisions( )
+                saveProvisions();
+                break;
+            case 4:// if the option is 4, call listProvisions( )
                 listProvisions();
                 break;
-            case 4://if the option is 4, call listTeam( )
+            case 5://if the option is 5, call listTeam( )
                 listTeam();
                 break;
-            case 5:// if the option is 5, display a goodbye message
+            case 6:// if the option is 6, display a goodbye message
                 System.out.println("\nReturning to game menu");
                 return;
         }
@@ -113,18 +121,56 @@ public void displayMenuView()
         System.out.println("\nThe tools you have are:...");
     }
     
+    public void saveProvisions(){
+        // declare a string to hold the file name
+        String fileName = "";
+        // declare a reference to a PrintWriter object
+        PrintWriter out = null;
+        // prompt the user for a file name, get and save the user’s input
+        System.out.println("Enter a list name to save:");
+        fileName = keyboard.next();
+        try {   
+            // create the PrintWriter object
+            out = new PrintWriter("pvl"+fileName+".txt");
+            // get a reference to the ArrayList you want to output
+            ArrayList<ListItem> provList = GameControl.getProv();
+            // output a heading for the report
+            out.println("\n     Provisions List\n");
+            out.printf("%-17s%10s","Item","Quantity\n");
+            out.printf("%-17s%10s","----","--------\n");
+            // use a for loop to get the data from the ArrayList and output it
+            for (ListItem i : provList){
+                out.printf("%-17s%10s", i.getName() , i.getNumber()+"\n");            
+            }
+            //out.close();
+            // print a success message
+            System.out.println("Success!");
+        } catch(IOException e) {
+            System.out.println("Saving File Error.");
+        } finally {
+            out.close();
+        }
+    }
 //    method to list provitions --L10
     public void listProvisions()
     {   
-        ArrayList<ListItem> provList = GameControl.getProv();
-        System.out.println("\nProvitions in City of Aaron:");
-        for (ListItem i : provList){ // Iterate through each item in 
-                                     //the provisions list
-            System.out.println(
-                    // The String.format() method aligns the text, for more info
-                    //see https://dzone.com/articles/java-string-format-examples
-                    String.format("%-17s", i.getName()) + i.getNumber()
-            );
+        // declare a string to hold the file name
+        String fileName = "";
+        // declare a reference to a BufferedReader object
+        BufferedReader in = null;
+        // prompt the user for a file name, get and save the user’s input
+        System.out.println("Enter a list name to print:");
+        fileName = keyboard.next();
+        try {
+            // create the BufferedReader object
+            in = new BufferedReader(new FileReader("pvl"+fileName+".txt"));
+            String line;
+            while ((line = in.readLine()) != null){
+                System.out.println(line);
+            }
+            in.close();
+        } catch(IOException e) {
+            System.out.println("Saving File Error.");
         }
     }   
     
